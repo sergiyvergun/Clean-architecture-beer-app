2fb8dbf0210eb8e9668ceeb6f39db93ce1fcf03e
Sergiy <sergiyvergun01@gmail.com>
2022-09-02 20:25:25 +0200

    Implement router

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+import 'features/beer_list/presentation/screens/beer_list_screen.dart';
+import 'utils/constants/routes.dart';
 
 void main() {
-  runApp(const BeerApp());
+  runApp(BeerApp());
 }
 
 class BeerApp extends StatelessWidget {
-  const BeerApp({Key? key}) : super(key: key);
+  BeerApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(title: 'Beer App',);
+    return MaterialApp.router(
+      title: 'Beer App',
+      routeInformationParser: _router.routeInformationParser,
+      routerDelegate: _router.routerDelegate,
+      routeInformationProvider: _router.routeInformationProvider,
+    );
   }
-}
 
+  final GoRouter _router = GoRouter(
+    routes: <GoRoute>[
+      GoRoute(
+        path: Routes.home,
+        builder: (BuildContext context, GoRouterState state) =>
+             const BeerListScreen(),
+      ),
+    ],
+  );
+}
